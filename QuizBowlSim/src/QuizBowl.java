72a29ef1b001a73b2187d20984ae982dbdf94848
David Fan <davidfan100@gmail.com>
2017-12-07 22:45:12 -0600

    Started quizbowlsim

--- QuizBowlSim/src/QuizBowl.java
+++ QuizBowlSim/src/QuizBowl.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +21,7 @@ public class QuizBowl {
 	/**
 	 * 
 	 */
-	private HashMap<String,String> question_Ans;
+	private HashMap<String,String> question_Ans = new HashMap<String,String>();
 	/**
 	 * 
 	 */
@@ -35,7 +37,7 @@ public class QuizBowl {
 	 */
 	public void startGame() throws IOException {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Hi there and welcome to the Quiz Bowl Simulator. Please enter the number of people playing this round (1-4)");
+		System.out.println("Hi there and welcome to the Quiz Bowl Simulator. Please enter the number of people playing this round (1-4)");
 		int numOfP = input.nextInt();
 		Player[] hold = new Player[numOfP];
 		for (int i = 0; i < numOfP; i++) {
@@ -44,12 +46,19 @@ public class QuizBowl {
 			hold[i] = new Player(s);
 		}
 		numOfPlayers = hold;
-		System.out.print("How many questions for this round (5,10,15)?");
-		numOfQuestions = input.nextInt();
+		int holdNums = 0;
+		while (true) {
+			if (holdNums >=5 && holdNums <= 20) {
+				numOfQuestions = holdNums;
+				break;
+			}
+			System.out.println("How many questions for this round (5-20)?");
+			holdNums = input.nextInt();
+		}
 		String tempS = "";
 		String line = null;
-		FileReader filereader = new FileReader("quizbowlquestions.txt");
-		BufferedReader br = new BufferedReader(filereader);
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+		                      new FileInputStream("quizbowlquestions.txt"), "UTF-8"));
 		while ((line = br.readLine()) != null) {
 			if (line.contains("*")) {
 				question_Ans.put(tempS, line.substring(1));
@@ -60,21 +69,26 @@ public class QuizBowl {
 			}
 		}
 		questions = new ArrayList<String>(question_Ans.keySet());
+		System.out.println(question_Ans);
 	}
 	public void displayQuestion() throws InterruptedException {
 		Thread t = new Thread();
 		Scanner input = new Scanner(System.in);
-		int index = (int)(Math.random() * 50);
-		String question = questions.get(index);
-		for(int i = 0; i < question.length(); i++) {
-			System.out.print(question.charAt(i));
-			t.sleep(75);
-		}
-		String answer = input.nextLine().toLowerCase();
-		if (question_Ans.get(questions.get(index)).indexOf(answer) >= 0) {
-			System.out.println("correct!");
-		} else {
-			System.out.println("Incorrect, the correct answer is " + question_Ans.get(questions.get(index)));
+		for (int i = 0; i < numOfPlayers.length; i++) {
+			int index = (int)(Math.random() * 50);
+			String question = questions.get(index);
+			for(int j = 0; j < question.length(); j++) {
+				System.out.print(question.charAt(j));
+				t.sleep(75);
+			}
+			String answer = input.nextLine().toLowerCase();
+			if (question_Ans.get(questions.get(index)).indexOf(answer) >= 0) {
+				numOfPlayers[i].changeScore(10);
+				System.out.println("correct!");
+			} else {
+				numOfPlayers[i].changeScore(-5);
+				System.out.println("Incorrect, the correct answer is " + question_Ans.get(questions.get(index)));
+			}
 		}
 	}
 	public void endGame() {
--- QuizBowlSim/src/QuizBowlGame.java
+++ QuizBowlSim/src/QuizBowlGame.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
 
 public class QuizBowlGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, InterruptedException {
+		QuizBowl game = new QuizBowl();
+		game.startGame();
+		for (int i = 0; i < game.getNumQ(); i++) {
+			game.displayQuestion();
+		}
 	}
 
 }
